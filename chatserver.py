82b798cd1deb44b233b87785fe48cfd34e4e8642
geer <gergo.kurucz@gmail.com>
2020-10-13 22:00:47 +0200

    v1.0

--- chatserver.py
+++ chatserver.py
@@ -1,18 +1,28 @@
-#framework importálása
-from flask import Flask, render_template
+#framework és a többi lom importálása
+from flask import Flask, render_template, json
+
 
 #asszinkron kéréskezelő importálása
 from flask_socketio import SocketIO, emit
 
-#a szerverosztály példányosítása
+
+#a kiszolgáló példányosítása
 app = Flask(__name__)
 
-socket = SocketIO()
 
-#indexoldal (kezdőlap) kérés kezelése
+#asszinkron (websocket) kiszolgáló példányosítása
+socket = SocketIO(app)
+
+
+#ebben fogjuk tárolni a felhasználóneveket és a session id-ket
+users = {}
+
+
+#indexoldal (kezdőlap) kérés kezelése - ez szinkron kérés-válasz!
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 #asszinkron kérések kezelése
 @socket.on('req_for_ansver')
@@ -23,7 +33,8 @@ def req(data):
 @socket.on('greet')
 def greet(data):
     print('SOMEBODY CONNECTED!')
-    emit('greet')
+    print('SENT: ' + str(data['message']))
+    emit('greet', json.dumps({'message':'HELLO FROM SERVER'}))
 
 
 #szerver indítása
--- static/js/controller.js
+++ static/js/controller.js
@@ -1,10 +1,13 @@
 socket = io();
 
-$(document).ready(function(){
+var username = '';
 
-    socket.emit('greet');
-    socket.on('greet', function(){
-        console.log('SERVER ANSVERED!');
-    });
+//the greeting message returns
+socket.on('greet', function(data){
+    console.log('SERVER ANSVERED!');
+});
 
+//send a greeting to the server
+$('#greet').click(function(){
+    socket.emit('greet', {message:'greeting from client'});
 });
