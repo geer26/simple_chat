bfb25d529fb76b86360e6af79aac784e8314f33a
geer26 <gergo.kurucz@gmail.com>
2020-10-15 17:23:27 +0200

    v1.4_DEPLOY

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
+# a Flask framework importálása
 from flask import Flask
+
+#asszinkron kéréskezelő importálása
 from flask_socketio import SocketIO, emit
 
+#a szerver példányosítása
 app = Flask(__name__)
 
+#az asszinkron kéréskezelő példányosítása és a szerverhez csatolása
 socket = SocketIO(app)
 
+#felhasználók listájának létrehozása
 users = []
 
+#a route-ok importálása
 from app import routes
--- chatserver.py
+++ chatserver.py
@@ -1,1 +1,5 @@
+#az "app" csomag importálása
+#minden mappa, amiben van "__init__.py", az csomag, és azt végre is hajtja.
+#így a kifejezés az app mappa __init__.py kódjában létrehozott szerverpéldányt indítja.
+
 from app import app
